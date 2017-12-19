83ffa0b917c978610132aca3a62089624fd8ca7a
JM <jean.d.marchal@gmail.com>
2017-12-19 10:07:07 -0500

    Not falling back anymore to the simList env to search for objects

--- fireSense_EscapePredict.R
+++ fireSense_EscapePredict.R
@@ -195,7 +195,7 @@ fireSense_EscapePredictRun <- function(sim)
     if (s <- sum(missing))
       stop(paste0(moduleName, "> '", allxy[missing][1L], "'",
                   if (s > 1) paste0(" (and ", s-1L, " other", if (s>2) "s", ")"),
-                  " not found in data objects nor in the simList environment."))
+                  " not found in data objects."))
     
     badClass <- unlist(lapply(allxy, function(x) is.vector(envData[[x]]) || is(envData[[x]], "RasterLayer")))
     
